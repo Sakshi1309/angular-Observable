95973a27a3bbe7838d5ba92d5c44774139f2ee42
Sakshi1309 <31957290+Sakshi1309@users.noreply.github.com>
2023-02-03 15:40:59 +0530

    2 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,13 +8,18 @@ import { HelloComponent } from './Components/hello.component';
 import { AppChild } from './Components/child/child.component';
 import { YourLocationComponent } from './Components/your-location/your-location.component';
 import { HttpClientModule } from '@angular/common/http';
-import { RouterModule } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
 import { ReversePipe } from './Pipes/demo.pipe';
 import {
   HighlightDirctive,
   TruncateDirective,
 } from './Directive/highlight.directive';
 
+const routes: Routes = [
+  { path: 'child', component: AppChild },
+  { path: 'location', component: YourLocationComponent },
+  { path: 'forms', component: HelloComponent },
+];
 @NgModule({
   imports: [
     BrowserModule,
