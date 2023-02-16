5fa5185d3c675164cc2b300574456f1eebb7bc06
Sakshi1309 <31957290+Sakshi1309@users.noreply.github.com>
2023-02-16 20:12:47 +0530

    1 file modified

--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { BehaviorSubject, forkJoin } from 'rxjs';
+import { BehaviorSubject, catchError, forkJoin } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -19,17 +19,36 @@ export class AppService {
 
   usingForkJoin() {
     const joinedAndDelayed$ = forkJoin({
-      users: this.http.get('https://jsonplaceholder.typicode.com/users'),
-      todos: this.http.get('https://jsonplaceholder.typicode.com/todos'),
-      albums: this.http.get('https://jsonplaceholder.typicode.com/albums'),
+      users: this.http.get('https://jsonplaceholder.typicode.com/users').pipe(
+        catchError(() => {
+          throw 'Error occured in users api';
+        })
+      ),
+      todos: this.http.get('https://jsonplaceholder.typicode.com/todos').pipe(
+        catchError(() => {
+          throw 'Error occured in todos api';
+        })
+      ),
+      albums: this.http.get('https://jsonplaceholder.typicode.com/albums').pipe(
+        catchError(() => {
+          throw 'Error occured in albums api';
+        })
+      ),
     });
-    joinedAndDelayed$.subscribe((data) => {
-      (this.users = data.users),
-        (this.albums = data.albums),
-        (this.todos = data.todos);
-      console.log(this.users);
-      console.log(this.albums);
-      console.log(this.todos);
+
+    joinedAndDelayed$.subscribe({
+      next(data) {
+        this.users = data.users;
+        this.albums = data.albums;
+        this.todos = data.todos;
+        console.log('data', data);
+      },
+
+      error(err) {
+        console.log(err);
+      },
+
+      complete() {},
     });
   }
 }
